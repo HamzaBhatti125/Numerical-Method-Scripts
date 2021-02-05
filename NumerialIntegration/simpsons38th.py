1af692be6fee0a0296cd1836e11c5bab710724ba
Hamza Bhatti <hbhati006@gmail.com>
2021-02-05 14:55:21 +0500

    Numerical integration Done

--- NumerialIntegration/simpsons38th.py
+++ NumerialIntegration/simpsons38th.py
@@ -1,7 +1,7 @@
 a = 0
-b = 1
+b = 6
 #values of a and b are lower and upper limit of integral
-no_of_points = 11 #user defined
+no_of_points = 7 #user defined
 
 n = no_of_points - 1
 h = (b-a)/n
@@ -10,8 +10,8 @@ x = []
 fx = []
 j = a
 
-def functionX(arg): #define function here
-    return 1/(1 + arg**2)
+def functionX(x): #define function here
+    return 1/(1+(x**2))
 
 for i in range(no_of_points):
     x.append(round(j,5))
@@ -22,8 +22,11 @@ print("x is: ", x)
 print("fx is: ", fx)
 
 for i in range(1,len(fx)-1):
-    fx[i] = 2*fx[i]
+    if i % 3 == 0:
+        fx[i] = 2*fx[i]    
+    else:
+        fx[i] = 3*fx[i]
 print("new Fx is: ", fx)
 
-result = round((h*sum(fx))/2,5)
+result = round((3*h*sum(fx))/8,5)
 print("The final result is: ", result)
--- NumerialIntegration/trapezoidal.py
+++ NumerialIntegration/trapezoidal.py
@@ -1,7 +1,7 @@
 a = 0
-b = 10
+b = 6
 #values of a and b are lower and upper limit of integral
-no_of_points = 5 #user defined
+no_of_points = 7 #user defined
 
 n = no_of_points - 1
 h = (b-a)/n
@@ -10,8 +10,8 @@ x = []
 fx = []
 j = a
 
-def functionX(arg): #define function here
-    return 3*(arg**2)
+def functionX(x): #define function here
+    return 1/(1+(x**2))
 
 for i in range(no_of_points):
     x.append(round(j,5))
@@ -22,11 +22,8 @@ print("x is: ", x)
 print("fx is: ", fx)
 
 for i in range(1,len(fx)-1):
-    if i % 2 == 1:
-        fx[i] = 4*fx[i]    
-    else:
-        fx[i] = 2*fx[i]
+    fx[i] = 2*fx[i]
 print("new Fx is: ", fx)
 
-result = round((h*sum(fx))/3,5)
+result = round((h*sum(fx))/2,5)
 print("The final result is: ", result)
